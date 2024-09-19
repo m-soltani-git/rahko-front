5334e80ad582d1c05aa322926297a79eac46f50d
Ali Morakabian <alimrk.isf@gmail.com>
2024-09-19 11:40:08 +0330

    Base Menu translates add

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,13 +1,45 @@
 const locales = {
   en: {
-    welcome: "Welcome Back...!",
-    app: "Main Page",
-    title: "Title",
+    //Base Menu
+    country: 'Country',
+    state: 'State',
+    city: 'City',
+    area: 'Area',
+    region: 'Region',
+    loaderType: 'Loader Type',
+    ladingFactor: 'Lading Factor',
+    stopCost: 'Stop Cost',
+    additionValue: 'Addition Value',
+    userStatus: 'User Status',
+    userType: 'User Type',
+    companyType: 'Company Type',
+    companyActivity: 'Company Activity',
+    companyBranch: 'Company Branch',
+
+    welcome: 'Welcome Back...!',
+    app: 'Main Page',
+    title: 'Title',
   },
   fa: {
-    welcome: "خوش آمدید...!",
-    app: "صفحه اصلی",
-    title: "عنوان",
+    //Base Menu
+    country: 'کشور',
+    state: 'استان',
+    city: 'شهر',
+    area: 'منطقه',
+    region: 'ناحیه',
+    loaderType: 'نوع بارگیری',
+    ladingFactor: 'عامل بار',
+    stopCost: 'هزینه توقف',
+    additionValue: 'ارزش افزوده',
+    userStatus: 'وضعیت کاربر',
+    userType: 'نوع کاربر',
+    companyType: 'نوع شرکت',
+    companyActivity: 'فعالیت شرکت',
+    companyBranch: 'شعبه شرکت',
+
+    welcome: 'خوش آمدید...!',
+    app: 'صفحه اصلی',
+    title: 'عنوان',
   },
 };
 
